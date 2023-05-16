32dd47e3653681bd187745a3ba27ec2153483201
Takayama <49364055+MGMCN@users.noreply.github.com>
2023-05-16 16:13:25 +0900

    add main.py utils.py requirements.txt

---
+++ utils.py
@@ -0,0 +1,96 @@
+import asyncio
+import aiohttp
+import json
+import os
+from tqdm import tqdm
+import re
+
+failed = []
+success = 0
+counts = 0
+
+
+def preprocessing(src_path, failed_path) -> list:
+    tasks = []
+    if src_path is not None:
+        with open(src_path, 'r') as file:
+            tasks.extend(json.load(file))
+    if failed_path is not None:
+        with open(failed_path, 'r') as file:
+            tasks.extend(json.load(file))
+    return tasks
+
+
+def generate_progress_bar():
+    fa = len(failed)
+    progress_bar = tqdm(total=counts, desc=f"Downloaded {success} Failed {fa} Left {counts - fa - success}")
+    return progress_bar
+
+
+def update_progress_bar(progress_bar):
+    fa = len(failed)
+    progress_bar.desc = f"Downloaded {success} Failed {fa} Left {counts - fa - success}"
+    progress_bar.update(1)
+
+
+def get_file_name(url) -> str:
+    path_parts = url.split('/')
+    file_name = path_parts[-1]
+    file_name = re.sub(r'[^\w\-_.()]', '_', file_name)
+    return file_name
+
+
+async def download(session,
+                   url,
+                   semaphore,
+                   save_dir,
+                   request_interval,
+                   chunk_size,
+                   progress_bar):
+    global success, failed
+    async with semaphore:
+        file_name = get_file_name(url)
+        save_path = f"{save_dir}/{file_name}"
+        try:
+            tqdm.write(f'👉 Downloading {file_name} now!')
+            async with session.get(url) as response:
+                await asyncio.sleep(request_interval)
+                with open(save_path, 'wb') as f:
+                    while True:
+                        chunk = await response.content.read(chunk_size)
+                        if not chunk:
+                            break
+                        f.write(chunk)
+            success += 1
+        except:
+            if os.path.exists(save_path):
+                os.remove(save_path)
+            failed.append(url)
+        finally:
+            update_progress_bar(progress_bar)
+
+
+async def main(tasks, args, progress_bar):
+    Max = args.max
+    semaphore = asyncio.Semaphore(Max)
+    save = args.save
+    interval = args.interval
+    chunk = args.chunk
+    session = aiohttp.ClientSession()
+    tasks = [asyncio.ensure_future(download(session, url, semaphore, save, interval, chunk, progress_bar)) for url in
+             tasks]
+    await asyncio.gather(*tasks)
+    await session.close()
+    fa = len(failed)
+    if fa > 0:
+        tqdm.write(f'❌ {fa} files failed to download!')
+        tqdm.write(
+            f'👉 Plz rerun with \"python3 main.py --save {save} --max {Max} --interval {interval} --chunk {chunk} --failed failed.json\"')
+        with open("failed.json", 'w') as f:
+            f.write(json.dumps(failed))
+    else:
+        tqdm.write('✅ All resources have been successfully downloaded!')
+
+
+def run(tasks, args, progress_bar):
+    asyncio.get_event_loop().run_until_complete(main(tasks, args, progress_bar))
